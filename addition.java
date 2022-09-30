e83cbf78f45bc4d4babcf2c32135e8b293dc8dd2
shivagandra <shivagandra9664@gmail.com>
2022-10-01 00:43:35 +0530

    add two numbers

---
+++ addition.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.List;
+
+public class Addition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+}
