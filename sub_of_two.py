0b380834a9d170f4a7280a9efcfb6f755318860e
Indrajeet Shelake <indravshelake95@gmail.com>
2022-10-02 23:29:04 +0530

    added substraction program

---
+++ sub_of_two.py
@@ -0,0 +1,12 @@
+# Substraction of two numbers in python
+
+#enter first number
+num1 = input()
+#enter second number
+num2 = input()
+
+#doing substraction operation
+sub_ans = num1-num2
+
+#substraction of first number-second number
+print(sub_ans)
